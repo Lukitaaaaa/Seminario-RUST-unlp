319ea744e189c7bb5148b03a2d804b0d3f3cbfbf
Lukitaaaaa <lucasomargallardo@gmail.com>
2025-05-01 13:54:59 -0300

    practica 2 ejercicio 3 y 4

--- practica_2/ejercicio4.rs
+++ practica_2/ejercicio4.rs
@@ -1,4 +1,4 @@
-fn hola(a: [i32;5])-> i32{
+fn cantidad_impares(a: [i32;5])-> i32{
     let mut cant:i32 = 0;
     for i in 0..a.len(){
         if a[i] % 2 != 0 {
@@ -10,7 +10,7 @@ fn hola(a: [i32;5])-> i32{
 }
 
 fn main(){
-    let array = [ 1, 4, 6, 8, 10 ];
+    let array: [i32;5] = [ 1, 4, 6, 8, 10 ];
     
-    println!("La cantidad de elementos impares es {}.", hola(array));
+    println!("La cantidad de elementos impares es {}.", cantidad_impares(array));
 }
