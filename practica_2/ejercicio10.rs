79501afbf2a78ce68359babeda82c052db7625e6
Lukitaaaaa <lucasomargallardo@gmail.com>
2025-05-01 20:12:16 -0300

    practica 2 ejercicio 10

--- practica_2/ejercicio10.rs
+++ practica_2/ejercicio10.rs
@@ -1,11 +1,11 @@
 fn main(){
     let array: [String;5] = ["Hola".to_string(), "Mundo".to_string(), "Rust".to_string(), "Es".to_string(), "Genial".to_string()];
     let li: i32 = 5;
-    let resultado = cant(array, li);
+    let resultado = cantidad_de_cadenas_mayor_a(array, li);
     println!("Cantidad de elementos con longitud mayor o igual a {}: {}", li, resultado);
 }
 
-fn cant(arr:[String;5], li:i32)->i32{
+fn cantidad_de_cadenas_mayor_a(arr:[String;5], li:i32)->i32{
     let mut cont:i32 = 0;
     for i in 0..arr.len(){
         if arr[i].len() as i32 >= li {
