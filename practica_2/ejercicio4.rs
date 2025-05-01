a86d4a2a95e155ca0143f17bc900b8958f296d61
Lukitaaaaa <lucasomargallardo@gmail.com>
2025-05-01 13:53:32 -0300

    practica 2 ejercicio 4

---
+++ practica_2/ejercicio4.rs
@@ -0,0 +1,16 @@
+fn hola(a: [i32;5])-> i32{
+    let mut cant:i32 = 0;
+    for i in 0..a.len(){
+        if a[i] % 2 != 0 {
+            cant += 1;
+        }
+    }
+
+    return cant;
+}
+
+fn main(){
+    let array = [ 1, 4, 6, 8, 10 ];
+    
+    println!("La cantidad de elementos impares es {}.", hola(array));
+}
