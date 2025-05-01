cf89ba134c1f331d9ac1a131ac6e818b99bf3f07
Lukitaaaaa <lucasomargallardo@gmail.com>
2025-05-01 19:53:10 -0300

    ejercicio 8

--- practica_2/ejercicio8.rs
+++ practica_2/ejercicio8.rs
@@ -1,4 +1,4 @@
-fn nigga(arr1:[f32;5], arr2:[f32;5])->[f32;5]{
+fn sumar_arreglos(arr1:[f32;5], arr2:[f32;5])->[f32;5]{
     let mut aux:[f32;5] = [0.0;5];
     
     for i in 0..aux.len(){
@@ -11,6 +11,6 @@ fn nigga(arr1:[f32;5], arr2:[f32;5])->[f32;5]{
 fn main(){
     let array1:[f32;5] = [2.0, 4.0, 6.0, 8.0, 10.0];
     let array2:[f32;5] = [2.0, 4.0, 6.0, 8.0, 10.0];
-    let resultado = nigga(array1,array2);
+    let resultado = sumar_arreglos(array1,array2);
     println!("RESULTADOS: {:?}", resultado);
 }
