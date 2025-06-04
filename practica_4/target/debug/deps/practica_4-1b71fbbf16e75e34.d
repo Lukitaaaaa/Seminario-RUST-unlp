ab5f0cba5481ceb46e62a8db57da23e315193f6e
Lukitaaaaa <lucasomargallardo@gmail.com>
2025-06-04 17:55:03 -0300

    practica 4 ejercicio 1

---
+++ practica_4/target/debug/deps/practica_4-1b71fbbf16e75e34.d
@@ -0,0 +1,5 @@
+C:\Users\lucas\OneDrive\Documentos\Licenciatura en Informatica\Seminario-RUST\Practica\practica_4\target\debug\deps\practica_4-1b71fbbf16e75e34.exe: src\main.rs
+
+C:\Users\lucas\OneDrive\Documentos\Licenciatura en Informatica\Seminario-RUST\Practica\practica_4\target\debug\deps\practica_4-1b71fbbf16e75e34.d: src\main.rs
+
+src\main.rs:
---
+++ practica_4/target/debug/deps/practica_4-414d6b6808c406cd.d
@@ -0,0 +1,5 @@
+C:\Users\lucas\OneDrive\Documentos\Licenciatura en Informatica\Seminario-RUST\Practica\practica_4\target\debug\deps\libpractica_4-414d6b6808c406cd.rmeta: src\main.rs
+
+C:\Users\lucas\OneDrive\Documentos\Licenciatura en Informatica\Seminario-RUST\Practica\practica_4\target\debug\deps\practica_4-414d6b6808c406cd.d: src\main.rs
+
+src\main.rs:
